6e928ba0e1ae794189e5416115fcb903fe8b5e81
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-10-10 14:22:43 +0100

    feat: update the header section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -36,7 +36,9 @@ const Header = () => {
         <div className="flex flex-col w-full  items-center gap-10">
           <div className="w-full flex justify-between items-center ">
             <div className="flex gap-8 items-center">
-              <h1 className="text-[24px] lg:text-[40px] font-bold">GalaxyTravel</h1>
+              <h1 className="text-[24px] lg:text-[40px] font-bold">
+                GalaxyTravel
+              </h1>
             </div>
             <div className="hidden lg:flex gap-5 text-[15px] text-[#051036]">
               <Link href={"/"} className="hover:text-[#bf2180]">
@@ -51,10 +53,9 @@ const Header = () => {
               <Link href={"/"} className="hover:text-[#bf2180]">
                 Blog
               </Link>
-              <Link href={"/"} className="hover:text-[#bf2180]">
+              <Link href={"/about-us"} className="hover:text-[#bf2180]">
                 About
               </Link>
-             
             </div>
             <div className="hidden lg:flex items-center gap-5">
               <Link
@@ -72,38 +73,51 @@ const Header = () => {
           </div>
 
           {showMenu && (
-            <div className="lg:hidden flex flex-col gap-5 left-0 w-full h-[100dvh] bg-white text-black  ">
+            <div className="lg:hidden fixed flex flex-col gap-5  text-center left-0 w-full h-[100dvh] bg-white text-black  ">
+              <div
+                onClick={handleShowMenu}
+                className="ml-auto text-right font-bold mb-5 p-5"
+              >
+                <span>X</span>
+              </div>
               <Link
                 href={"/"}
-                className="  hover:bg-gray-100 hover:text-[#3554d1]"
+                className="  hover:bg-gray-100 hover:text-[#bf2180]"
               >
                 Hotels
               </Link>
               <Link
                 href={"/"}
-                className="hover:bg-gray-100 hover:text-[#3554d1]"
+                className="hover:bg-gray-100 hover:text-[#bf2180]"
               >
                 Flights
               </Link>
               <Link
                 href={"/"}
-                className="hover:bg-gray-100 hover:text-[#3554d1]"
+                className="hover:bg-gray-100 hover:text-[#bf2180]"
               >
                 Tours
               </Link>
               <Link
                 href={"/"}
-                className=" hover:bg-gray-100 hover:text-[#3554d1]"
+                className=" hover:bg-gray-100 hover:text-[#bf2180]"
               >
                 Blog
               </Link>
               <Link
                 href={"/about-us"}
-                className=" hover:bg-gray-100 hover:text-[#3554d1]"
+                className=" hover:bg-gray-100 hover:text-[#bf2180]"
               >
                 About
               </Link>
-             
+              <div className="hidden lg:flex items-center gap-5">
+                <Link
+                  href={"/signup"}
+                  className="bg-[#bf2180] text-white rounded-[8px] px-[30px] py-[10px] text-[14px] hover:bg-[#bf2180] hover:transition-all"
+                >
+                  Get started
+                </Link>
+              </div>
             </div>
           )}
         </div>
--- src/container/home/hero/hero.jsx
+++ src/container/home/hero/hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   };
 
   return (
-    <div className="mt-[50px] lg:mt-[85px]  bg-[url('/images/hero-bg.png')] h-[650px] lg:h-[400px] flex justify-between w-full">
+    <div className="mt-[100px] lg:mt-[85px]  bg-[url('/images/hero-bg.png')] h-[650px] lg:h-[400px] flex justify-between w-full">
       <div className="flex items-center justify-center w-[95%] mx-[2.5%] lg:w-1/2 lg:mx-[25%] ">
         <Card className="border-2 border-[#E6D2E8]  shadow-[0_0_40px_0,rgba(230,210,232,1)] rounded-[13px] lg:p-[3%]  p-[5%] w-full  flex flex-col gap-10">
           <div className="flex gap-1 items-center">
