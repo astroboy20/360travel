9df8d9688615d39d3ee6a581d8983b8a0af13eea
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-10-12 01:44:16 +0100

    update

--- src/container/dashboard/bookings.jsx
+++ src/container/dashboard/bookings.jsx
@@ -8,8 +8,8 @@ import {
 
 const Bookings = () => {
   return (
-    <div className=" w-full h-full">
-      <div className="flex flex-col gap-10 p-[6%] bg-gray-100">
+    <div className="flex flex-col gap-10  w-full h-screen bg-gray-100">
+      <div className="flex flex-col gap-10 p-[6%] ">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-3xl font-semibold text-gray-700">Hi, Tolulope</h1>
         </div>
@@ -40,10 +40,9 @@ const Bookings = () => {
           </div>
         </div>
 
-        <div className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center">
-            <FaDollarSign size={40} className="text-3xl text-[#bf2180] mb-4" />
-            <h2 className="text-2xl font-bold">$0</h2>
-            <p className="text-gray-500">Your balance</p>
+        <div className="bg-white p-6 rounded-lg shadow-md flex flex-col">
+           <h1 className="text-[32px] font-bold">Recent Bookings</h1>
+        
           </div>
       </div>
     </div>
